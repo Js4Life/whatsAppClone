3f346a3ee18562d5199931cc9574a2567ecd12d3
Raghavendra <raghavendra@Raghavendras-MacBook-Pro.local>
2021-01-23 13:16:20 +0530

    chatRoommsg

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -9,7 +9,7 @@ import MainTabNavigator from './MainTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
 import Colors from '../constants/Colors';
 import { Octicons, MaterialCommunityIcons, MaterialIcons, FontAwesome5 } from '@expo/vector-icons'
-import ChatRoomScreen from '../screens/ChatRoom';
+import ChatRoomScreen from '../screens/ChatRoomScreen';
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
