5c31889283938805116494335b1ab0943efbb0c0
Raghavendra <raghavendra@Raghavendras-MacBook-Pro.local>
2021-01-23 12:31:49 +0530

    Basic chat room screen

--- components/ChatListItem/index.tsx
+++ components/ChatListItem/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableWithoutFeedback } from 'react-native';
 import { ChatRoom } from '../../types';
 var moment = require('moment');
 import styles from './style';
+import {useNavigation} from '@react-navigation/native'
 
 export type ChatListItemProps = {
     chatRoom: ChatRoom
@@ -11,20 +12,33 @@ const ChatListItem = (props: ChatListItemProps) => {
 
     const { chatRoom } = props;
     const user = chatRoom.users[1]
-    console.log('8845', user.imageUri)
+  
+
+
+    const navigation = useNavigation();
+    const onclick = () =>{
+       // console.warn(`Clicked on ${user.name}`)
+        navigation.navigate('ChatRoom',{ 
+        id: chatRoom.id,
+        name:user.name
+        })
+    }
 
     return (
-        <View style={styles.container}>
-            <View style={styles.lefContainer}>
-                <Image source={{ uri: user.imageUri }} style={styles.avatar} />
-                <View style={styles.midContainer}>
-                    <Text style={styles.username}>{user.name}</Text>
-                    <Text numberOfLines={2} style={styles.lastmsg}>{chatRoom.lastMessage.content}</Text>
+        <TouchableWithoutFeedback onPress={onclick}>
+            <View style={styles.container}>
+                <View style={styles.lefContainer}>
+                    <Image source={{ uri: user.imageUri }} style={styles.avatar} />
+                    <View style={styles.midContainer}>
+                        <Text style={styles.username}>{user.name}</Text>
+                        <Text numberOfLines={2} style={styles.lastmsg}>{chatRoom.lastMessage.content}</Text>
+                    </View>
                 </View>
+                <Text>{moment(chatRoom.lastMessage.createdAt).format("DD/MM/YYYY")}</Text>
+                {/* <Text style={styles.time}>Yesterday</Text> */}
             </View>
-            <Text>{moment(chatRoom.lastMessage.createdAt).format("DD/MM/YYYY")}</Text>
-            {/* <Text style={styles.time}>Yesterday</Text> */}
-        </View>
+        </TouchableWithoutFeedback>
+
     )
 };
 
--- components/ChatListItem/style.ts
+++ components/ChatListItem/style.ts
@@ -28,7 +28,7 @@ const styles = StyleSheet.create({
         
     },
     time:{
-        fontSize:16,
+        fontSize:14,
         color:'grey' 
     }
 })
